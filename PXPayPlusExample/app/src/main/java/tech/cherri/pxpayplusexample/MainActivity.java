aaf2e62bcd114677a348a355790596e138b1f2fe
CostaPeng <costapeng@cherricorp.com>
2025-04-30 08:47:40 +0800

    add check payment url

--- PXPayPlusExample/app/src/main/java/tech/cherri/pxpayplusexample/MainActivity.java
+++ PXPayPlusExample/app/src/main/java/tech/cherri/pxpayplusexample/MainActivity.java
@@ -211,7 +211,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         if (v.getId() == R.id.btnRedirectUrl) {
-            tpdPXPayPlus.redirectWithUrl(paymentUrl);
+            if (null != paymentUrl) {
+                tpdPXPayPlus.redirectWithUrl(paymentUrl);
+            }
         }
     }
 
