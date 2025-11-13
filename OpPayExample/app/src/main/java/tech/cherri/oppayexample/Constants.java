f0246589812b1ad0e06f50f219e03d97af6ba267
CostaPeng <costapeng@cherricorp.com>
2025-11-13 17:33:09 +0800

    add lib for oppay

---
+++ OpPayExample/app/src/main/java/tech/cherri/oppayexample/Constants.java
@@ -0,0 +1,17 @@
+package tech.cherri.oppayexample;
+
+
+import tech.cherri.tpdirect.api.TPDServerType;
+
+public class Constants {
+
+    public static final TPDServerType SERVER_TYPE = TPDServerType.Sandbox;
+    public static final String TAPPAY_DOMAIN = "https://sandbox.tappaysdk.com/tpc";
+    public static final String TAPPAY_PAY_BY_PRIME_URL = "/payment/pay-by-prime";
+
+    public static final String PARTNER_KEY = "your partner key";
+    public static final String APP_KEY = "your app key";
+    public static final Integer APP_ID = 0;
+    public static String MERCHANT_ID = "your merchant id";
+
+}
